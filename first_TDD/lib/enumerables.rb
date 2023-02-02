73f1fbb5bc26ad3ac82b6ddef6ab55e486145b30
Olga Borovikova <olga.sleepless@gmail.com>
2023-02-02 15:24:23 -0500

    two_sum spec method

--- first_TDD/lib/enumerables.rb
+++ first_TDD/lib/enumerables.rb
@@ -1,8 +1,23 @@
 def my_uniq(arr)
   hash = Hash.new(0)
   arr.each { |ele| hash[ele] += 1 }
+  #arr[0]=3
   hash.keys
+
   #   arr.uniq
 end
 
-arr = [1, 2, 1, 3, 3]
+#arr = [1, 2, 1, 3, 3]
+
+class Array
+  def two_sum
+    res = []
+    (0..self.length-2).each do |i|
+      (i+1..self.length-1).each do |j|
+        res << [i,j] if self[i]+self[j] == 0
+      end
+    end
+    return res
+    #res.empty? ? [1] : res 
+  end
+end
--- first_TDD/spec/enumerables_spec.rb
+++ first_TDD/spec/enumerables_spec.rb
@@ -8,15 +8,40 @@ describe "#my_uniq" do
     end
   end
 
-  # context "valid parameter" do
-  #   it "parameter is an Array" do
-  #     expect(my_uniq([1, 2, 1, 3, 3])).to_not raise_error
-  #   end
-  # end
+  context "valid parameter" do
+    it "parameter is an Array" do
+      array = [1, 2, 3]
+      expect{my_uniq(array)}.to_not raise_error
+    end
+  end
 
   it "should not use built_in Array#uniq method" do
     array = [1, 2, 3]
     expect(array).to_not receive(:uniq)
     my_uniq(array)
+  end
+
+  it "not modify original array" do 
+    array = [1, 2, 3,3,2]
+    array2 = array.dup
+    my_uniq(array)
+    expect(array).to eq(array2)
+  end
+end
+
+describe "#two_sum" do
+  subject(:array) {[-1,0,2,-2,1]}
+  let(:array2) {[5, 5, 3, 1]}
+  
+  it "dictionary-wise order" do
+    expect(array.two_sum).to eq ([[0,4], [2,3]])
+  end
+
+  it "returns [] when no pair is found" do
+    expect(array2.two_sum).to eq([])
+  end
+
+  it "won't find spurious zero pairs" do
+    expect(array2.two_sum).to eq([])
   end
 end
